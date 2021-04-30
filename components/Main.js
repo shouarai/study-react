5ac766805c9d2d23a0bce5929128b2f50b9e03fb
shouarai <shouarai@gmail.com>
2021-04-30 14:52:57 +0900

    💄 styleファイルの分割

--- components/Main.js
+++ components/Main.js
@@ -1,17 +1,15 @@
-import styles from "../styles/Home.module.css";
+import classes from "./Main.module.css";
 import { Headline } from "../components/Headline";
 import { Links } from "./Links";
 
 export function Main(props) {
 	return (
-		<div className={styles.container}>
-			<main className={styles.main}>
-				<Headline page={props.page}>
-					{/* コンポーネントはchildrenで渡すことが多い */}
-					<code className={styles.code}>pages/{props.page}.js</code>
-				</Headline>
-				<Links />
-			</main>
-		</div>
+		<main className={classes.main}>
+			<Headline page={props.page}>
+				{/* コンポーネントはchildrenで渡すことが多い */}
+				<code className={classes.code}>pages/{props.page}.js</code>
+			</Headline>
+			<Links />
+		</main>
 	);
 }

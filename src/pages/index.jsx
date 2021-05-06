0c8be062841ef15dcddce9ed2615b2cb527c877d
shouarai <shouarai@gmail.com>
2021-05-07 06:55:18 +0900

    ⚗ usecallbackのテスト

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,15 +3,25 @@ import styles from "../styles/Home.module.css";
 import { Footer } from "../components/Footer";
 import { Main } from "../components/Main";
 import { Header } from "../components/Header";
+import { useCallback } from "react";
 
 export default function Home() {
+	const foo = 1;
+
+	const handleClick = useCallback((e) => {
+		console.log(e.target.href);
+		e.preventDefault();
+		alert(foo);
+	}, []);
 	return (
 		<div className={styles.container}>
 			<Head>
 				<title>Index Page</title>
 			</Head>
 			<Header />
-
+			<a href='/about' onClick={handleClick}>
+				ボタン
+			</a>
 			<Main page='index' />
 			<Footer />
 		</div>

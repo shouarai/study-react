951171a8867d1e81212139ec058b2a11695aef08
shouarai <shouarai@gmail.com>
2021-05-27 06:59:47 +0900

    ✨ usetateで文字列と真偽値について解説

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,12 +7,23 @@ import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
 	const [count, setCount] = useState(1);
+	const [text, setText] = useState("");
+	const [isShow, setIsShow] = useState(true);
 
 	const handleClick = useCallback(() => {
 		if (count < 10) {
 			setCount((count) => count + 1);
 		}
 	}, []);
+
+	const handleChange = useCallback((e) => {
+		if (e.target.value.length > 5) {
+			alert("5文字以内にしてください");
+			return;
+		}
+		setText(e.target.value.trim());
+	}, []);
+
 	useEffect(() => {
 		// console.log(`マウント時：${count}`);
 		document.body.style.backgroundColor = "lightblue";
@@ -22,16 +33,20 @@ export default function Home() {
 		};
 	}, []);
 
+	const handleDisplay = useCallback(() => {
+		setIsShow((isShow) => !isShow);
+	}, []);
+
 	return (
 		<div className={styles.container}>
 			<Head>
 				<title>Index Page</title>
 			</Head>
 			<Header />
-			<h1>{count}</h1>
-			<button href='/about' onClick={handleClick}>
-				ボタン
-			</button>
+			{isShow ? <h1>{count}</h1> : null}
+			<button onClick={handleClick}>ボタン</button>
+			<button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+			<input type='text' value={text} onChange={handleChange} />
 			<Main page='index' />
 			<Footer />
 		</div>

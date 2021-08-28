ead715583859f7feb1049244e496b8aa80eff795
shouarai <shouarai@gmail.com>
2021-08-28 16:51:51 +0900

    🚨 フッターのlintの設定

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -7,7 +7,7 @@ export const Footer = () => {
 				href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
 				target='_blank'
 				rel='noopener noreferrer'>
-				Thanks by{" "}
+				Thanks by {/* eslint-disable-next-line @next/next/no-img-element */}
 				<img src='/vercel.svg' alt='Vercel Logo' className={classes.logo} />
 			</a>
 		</footer>
